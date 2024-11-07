3ff6b5007b09cb3e7867cfa65dd1d9eb35f7cead
mohamed271220 <mohamedaprahym@gmail.com>
2024-11-07 17:53:28 +0200

    ready for deployment

--- frontend/api/index.js
+++ frontend/api/index.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:3000/api/v1',
+    // backend link from .env.local
+    baseURL: process.env.NEXT_PUBLIC_BACKEND_URL,
     withCredentials: true,
 });
 export const getBlogPosts = async () => {
