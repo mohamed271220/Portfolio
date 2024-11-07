2e4b9b663c1dc0547c863bdc916f07a91b64fd39
mohamed271220 <mohamedaprahym@gmail.com>
2024-11-07 19:00:00 +0200

    finished wallahi

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -1,39 +1,10 @@
-import Certifications from '@/components/Certifications';
-import EducationSection from '@/components/Educations';
-import ExperiencesSection from '@/components/ExperiencesSection';
-import Footer from '@/components/Footer';
-import { HeroSection } from '@/components/HeroSection';
-import Projects from '@/components/Projects';
-import Skills from '@/components/Skills';
-import { fetchData } from '@/lib/data';
-import { notFound } from 'next/navigation';
-
+import Main from '@/components/Main';
 
 export const metadata = {
   title: "Mohamed Magdy",
   description: "A personal portfolio for the full-stack developer Mohamed Magdy",
 }
 
-export default async function Home() {
-  try {
-    const data = await fetchData();
-    if (!data) {
-      notFound();
-    }
-    return (
-      <>
-        <HeroSection name={data.me.name} profilePicture={data.me.profilePicture} currentPosition={data.me.currentPosition} resume={data.me.resume} />
-        <ExperiencesSection experiencesData={data.experiences} />
-        <Projects projects={data.projects} categories={data.categories} />
-        <Skills skills={data.skills} />
-        <EducationSection educationData={data.educations} type="education" />
-        <Certifications certifications={data.certifications} />
-        <Footer data={data.me} />
-      </>
-    );
-  } catch (error) {
-    console.error('Error loading blog posts:', error);
-    notFound();
-  }
-
+export default function Home() {
+  return <Main />
 }
